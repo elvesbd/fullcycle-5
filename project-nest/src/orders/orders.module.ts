50a2549ed8321fc0fb0a17b42f57a0bbf0a8ceb7
elvesbd <elvesbd@yahoo.com>
2021-12-08 12:52:56 -0300

    refactoring folders and files

--- project-nest/src/orders/orders.module.ts
+++ project-nest/src/orders/orders.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
+import { OrdersController } from './controllers/orders.controller';
+import { OrdersService } from './services/orders.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
 })
 export class OrdersModule {}
